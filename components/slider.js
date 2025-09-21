70141433657a49841c2d60105b56fdabd9a150f5
joo <122433286+kepten31415926@users.noreply.github.com>
2025-09-21 16:42:41 +0900

    infinite carousel but with individual images

--- components/slider.js
+++ components/slider.js
@@ -1,16 +1,53 @@
 document.querySelectorAll('.carousel').forEach(carousel => {
     const container = carousel.querySelector('.row-container');
-    const image = carousel.querySelectorAll('.row-container img');
+    const images = carousel.querySelectorAll('.row-container a');
     const nextBtn = carousel.querySelector('.next');
     const prevBtn = carousel.querySelector('.prev');
-
     let i = 1;
-    let imageWidth = image[0].offsetWidth + 10;
+
+    let imageWidth = images[0].offsetWidth + 10;
+    //infinite carousel feature
+    //document selector all treats html element like an array(?)
+    const firstClone = images[0].cloneNode(true);
+    const lastClone = images[images.length -1].cloneNode(true);
+    const firstImage = container.querySelector('a');
+    firstClone.id = 'first-clone';
+    lastClone.id = 'last-clone';
+
+    container.appendChild(firstClone); //so first clone appears at the end
+    container.insertBefore(lastClone, firstImage); //so last clone can show up before first element
+    
+    const allImages = carousel.querySelectorAll(".row-container a");
+    
+    //initial position
+    container.style.transform = `translateX(${-imageWidth * i}px)`;
 
     nextBtn.addEventListener("click", ()=>{
-        container.scrollBy({left: imageWidth,behavior:"smooth"});
+        if(i>=allImages.length-1) return;
+        i++;
+        container.style.transition = 'transform 0.4s ease-in-out';
+        container.style.transform = `translateX(${-imageWidth*i}px)`;
     });
     prevBtn.addEventListener("click",()=>{
-        container.scrollBy({left: -imageWidth,behavior:"smooth"});
+        if(i<=0) return;
+        i--;
+        container.style.transition = 'transform 0.4s ease-in-out';
+        container.style.transform = `translateX(${-imageWidth*i}px)`;
+
     });
+
+    //loop reset
+    container.addEventListener('transitionend', ()=>{
+        const current = allImages[i];
+        if (allImages[i].id==='first-clone'){
+            container.style.transition='none';
+            i=1;
+            container.style.transform=`translateX(${-imageWidth*i}px)`;
+        }
+        if (current.id ==='last-clone'){
+            container.style.transition = 'none';
+            i = allImages.length-2;
+            container.style.transform=`translateX(${-imageWidth*i}px)`;
+        }
+    })
 })
